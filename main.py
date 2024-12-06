8cc0b804fbfdb07bb45f47dda12ac9e28390e3c7
matt-monnie <matt_monnie@hotmail.com>
2024-12-05 21:41:45 -0500

    Update logic to utilize groups
    Update the logic in the game loop to utilize pygame groups. This will
    simplify the calls to methods

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    print ("Starting asteroids!")
-    print (f"Screen width: {SCREEN_WIDTH}")
-    print (f"Screen height: {SCREEN_HEIGHT}")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     game_clock = pygame.time.Clock()
     dt = 0
 
     while(True):
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
