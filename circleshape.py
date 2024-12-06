43875a282edf3f9a10447b2e0eb31b892cf74837
matt-monnie <matt_monnie@hotmail.com>
2024-12-05 22:25:14 -0500

    Add asteroid field and collisions
    Add a random spawning asteroid field. Add collision between asteroid
    and player object

--- circleshape.py
+++ circleshape.py
@@ -3,7 +3,7 @@ import pygame
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        if hasattr(self, "container"):
+        if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
             super().__init__()
@@ -21,3 +21,7 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+
+    def collision(self, other):
+        return  (self.position.distance_to(other.position) <= self.radius + other.radius)
--- main.py
+++ main.py
@@ -2,18 +2,28 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     game_clock = pygame.time.Clock()
     dt = 0
@@ -22,6 +32,10 @@ def main():
         screen.fill((0,0,0))
         for obj in updatable:
             obj.update(dt)
+        for obj in asteroids:
+            if obj.collision(player):
+                print ("Game over!")
+                sys.exit()
         for obj in drawable:
             obj.draw(screen)
         for event in pygame.event.get():
