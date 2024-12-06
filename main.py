39112829d890a8c72eb5a2fcda3282ee7745982d
matt-monnie <matt_monnie@hotmail.com>
2024-12-05 20:54:21 -0500

    Add the player object
    Add the player gameObject class. The class overwrites the base class
    allowing for the player to look like a triangle, while keeping the
    base cirecle shape

--- main.py
+++ main.py
@@ -3,10 +3,12 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print ("Starting asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
@@ -16,6 +18,7 @@ def main():
 
     while(True):
         screen.fill((0,0,0))
+        player.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
