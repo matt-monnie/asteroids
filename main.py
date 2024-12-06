e7d7581a10cee9d4cb41041e609899d8f8d3d9a5
matt-monnie <matt_monnie@hotmail.com>
2024-12-05 20:39:22 -0500

    Add game tick rate limit
    Add ability to limit the game tick rate to prevent excessive CPU usage

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ def main():
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     while(True):
         screen.fill((0,0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
